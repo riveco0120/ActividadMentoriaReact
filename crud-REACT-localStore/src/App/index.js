e8a5ecf43dc4e504dd2e6c2d2a8b1e3245e0c4e5
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-04 21:27:48 -0500

    Creando formulario
    Se agregaron formulario para inicio de seccion

--- crud-REACT-localStore/src/App/index.js
+++ crud-REACT-localStore/src/App/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { TodoProvider } from '../components/TodoContext/index';
-import { AppUI } from './AppUI';
-
+import React from "react";
+import { TodoProvider } from "../components/TodoContext/index";
+import { Formulario } from "../components/Formulario/Formulario";
 function App() {
   return (
     <TodoProvider>
-      <AppUI/>
+      <diV className="container">
+        <h1>FORMULARIO DE INGRESO</h1>
+        <Formulario />
+      </diV>
     </TodoProvider>
   );
 }
